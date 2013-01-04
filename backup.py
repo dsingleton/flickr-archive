3a37c9de04001e5cefac52ec3f8104d8c1832ef0
David Singleton <david@dsingleton.co.uk>
2013-01-04 13:28:45 +0000

    re-structure the script to use plac for simpler command line parsing

--- backup.py
+++ backup.py
@@ -1,20 +1,51 @@
-#!/usr/bin/python
-import flickrapi, urllib, config, sys
+#!/usr/bin/env python
 
-flickr = flickrapi.FlickrAPI(config.api_key, config.api_secret)
-flickr.token.path = '.tokens'
+# Plac bootstrap https://github.com/DarwinAwardWinner/python-script-template/blob/master/python-script.py
+import plac, logging
 
-dest = sys.argv[1]
+# Export dependencies
+import flickrapi, urllib
+from config import api_key, api_secret, user_id as flickr_user_id
 
-# Super simple Authentication - http://stuvel.eu/media/flickrapi-docs/documentation/#authentication
-(token, frob) = flickr.get_token_part_one(perms='write')
-if not token: raw_input("Press ENTER after you authorized this program")
-flickr.get_token_part_two((token, frob))
 
-def process_photo(photo):
-    localfile = '%s/%s - %s.%s' % (dest, photo.get('id'), photo.get('title', '').replace('/', ':'), photo.get('originalformat'))
+@plac.annotations(
+    # arg=(helptext, kind, abbrev, type, choices, metavar)
+    destination=("The directory to output the photos and metadata", "positional", None, str, None, "destination"),
+    quiet=("Do not print informational messages.", "flag", "q"),
+    verbose=("Print debug messages that are probably only useful if something is going wrong.", "flag", "v"),
+)
+def main(destination, quiet=False, verbose=False):
+    """Backup a users photos """
+    if quiet:
+        logging.basicConfig(level=logging.WARN)
+    elif verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+    
+    # Setup API and authenticate
+    flickr = authenticate(flickrapi.FlickrAPI(api_key, api_secret))
+    
+    for photo in flickr.walk(user_id=flickr_user_id, per_page=10, extras='original_format, url_o, date_taken'):
+        print photo.get('id')
+        # process_photo(destination, photo)
+
+def process_photo(destination, photo):
+    localfile = '%s/%s - %s.%s' % (destination, photo.get('id'), photo.get('title', '').replace('/', ':'), photo.get('originalformat'))
     urllib.urlretrieve(photo.get('url_o'), localfile)
 
-for photo in flickr.walk(user_id=config.user_id, per_page=10, extras='original_format, url_o, date_taken'):
-    process_photo(photo)
-    
+def authenticate(flickr):
+    """"Super simple Authentication
+
+    http://stuvel.eu/media/flickrapi-docs/documentation/#authentication"""
+
+    (token, frob) = flickr.get_token_part_one(perms='write')
+    if not token:
+        raw_input("Press ENTER after you authorized this program")
+
+    flickr.get_token_part_two((token, frob))
+
+    return flickr
+
+if __name__=="__main__":
+    plac.call(main)
