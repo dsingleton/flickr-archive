6e2893f61ae290b7e2ea59f10cc16857de0fa42a
David Singleton <david@dsingleton.co.uk>
2013-01-06 15:54:44 +0000

    multi threaded photo fetching

--- backup.py
+++ backup.py
@@ -11,9 +11,10 @@ from config import api_key, api_secret, user_id as flickr_user_id
     # arg=(helptext, kind, abbrev, type, choices, metavar)
     destination=("The directory to output the photos and metadata", "positional", None, str, None, "destination"),
     quiet=("Do not print informational messages.", "flag", "q"),
+    fetchphotos=("Fetch the photos.", "flag", "f"),
     verbose=("Print debug messages that are probably only useful if something is going wrong.", "flag", "v"),
 )
-def main(destination, quiet=False, verbose=False):
+def main(destination, fetchphotos = False, quiet=False, verbose=False):
     """Backup a users photos """
     
     if quiet:
@@ -26,32 +27,41 @@ def main(destination, quiet=False, verbose=False):
     # Setup API and authenticate
     flickr = authenticate(flickrapi.FlickrAPI(api_key, api_secret))
     photos = {}
+
+    logging.warn("Fetching photo metadata")
     
-    for photo in flickr.walk(user_id=flickr_user_id, per_page=100, extras='description, url_o, tags, date_taken, views'):
-        logging.warn("Processing photo: %s" % (photo.get('id')))
+    for photo in flickr.walk(user_id=flickr_user_id, per_page=250, extras='description, url_o, tags, date_taken, views, original_format'):
 
         photos[photo.get('id')] = {
-            'original_url': photo.get('url_o'),
             'title': photo.get('title'),
             'description': photo.find('description').text,
+            'file': '%s/%s.%s' % (destination, photo.get('id'), photo.get('originalformat')),
+            'original_url': photo.get('url_o'),
             'width': photo.get('width_o'),
             'height': photo.get('height_o'),
             'date_taken': photo.get('datetaken'),
             'tags': photo.get('tags').split(),
-            'views': photo.get('views')
+            'views': photo.get('views'),
+            'flickr_id': photo.get('id'),
         }
+        
     
+    logging.warn("Writing photo metadata")
     with io.open('%s/photos.json' % (destination), 'w+') as photos_file:
         photos_file.write(unicode(json.dumps(photos)))
 
+    if fetchphotos:
 
-def write_photo(destination, photo):
-    localfile = '%s/%s.%s' % (destination, photo.get('id'), photo.get('originalformat'))
-    if not os.path.isfile(localfile):
-        logging.warn("Writing photo for: %s" % (photo.get('id')))
-        urllib.urlretrieve(photo.get('url_o'), localfile)
+        from multiprocessing import Pool
+        pool = Pool(5)
+        pool.map(fetch_image, photos.values())
+
+def fetch_image(photo):
+    if not os.path.isfile(photo['file']):
+        logging.warn("Writing photo for: %s" % (photo.get('flickr_id')))
+        urllib.urlretrieve(photo.get('original_url'), photo['file'])
     else:
-        logging.warn("Skipping photo for: %s" % (photo.get('id')))
+        logging.warn("Skipping photo for: %s" % (photo.get('flickr_id')))
 
 def authenticate(flickr):
     """"Super simple Authentication
